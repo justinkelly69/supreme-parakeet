cc02a1016d0ef8b2921d5dc6e57b93723d5fd9f2
Justin Kelly <justinkelly@justinkelly.net>
2025-09-12 16:49:02 +0100

    Initialized

---
+++ sql/create-world.sql
@@ -0,0 +1,59 @@
+DROP SCHEMA IF EXISTS world CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS world;
+
+DROP TABLE IF EXISTS world.countries;
+
+CREATE TABLE
+    IF NOT EXISTS world.countries (
+        "id" CHAR(2) NOT NULL UNIQUE,
+        "is_enabled" BOOLEAN NOT NULL DEFAULT FALSE,
+        CONSTRAINT "fk_countries" FOREIGN KEY ("id") REFERENCES iso.countries ("id")
+    );
+
+INSERT INTO
+    world.countries (id)
+SELECT
+    "id"
+FROM
+    iso.countries
+ORDER BY
+    "name";
+
+GRANT SELECT, UPDATE ON world.countries TO public;
+
+DROP VIEW IF EXISTS public.country_details;
+
+CREATE VIEW
+    public.country_details AS
+SELECT
+    co."id",
+    co."continent_id",
+    cn."name" AS "continent_name",
+    co."name",
+    co."flag",
+    co."tld",
+    co."prefix",
+    co."is_eu",
+    wc."is_enabled",
+    JSON_AGG (
+        json_build_object ('id', lg."id", 'name', lg."name", 'is_enabled', lg."is_enabled")
+    ) AS languages,
+    JSON_AGG (
+        json_build_object ('id', cu."id", 'name', cu."name")
+    ) AS currencies
+FROM
+    iso.countries co
+    LEFT JOIN iso.continents cn ON cn."id" = co."continent_id"
+    LEFT JOIN iso.country_languages cl ON co."id" = cl."country_id"
+    LEFT JOIN iso.languages lg ON lg."id" = cl."language_id" 
+    LEFT JOIN iso.country_currencies cc ON co."id" = cc."country_id"
+    LEFT JOIN iso.currencies cu ON cu."id" = cc."currency_id" 
+    LEFT JOIN world.countries wc ON wc."id" = co."id"
+GROUP BY
+    co."id",
+    cn."name",
+    wc."is_enabled"
+ORDER BY
+    co."name";
+
---
+++ sql/create_schemas.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA world;
+CREATE SCHEMA iso;
+
+
+GRANT USAGE ON SCHEMA world TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA world TO anon, authenticated, service_role;
+GRANT ALL ON ALL ROUTINES IN SCHEMA world TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA world TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA world GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA world GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA world GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
+
+GRANT USAGE ON SCHEMA iso TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA iso TO anon, authenticated, service_role;
+GRANT ALL ON ALL ROUTINES IN SCHEMA iso TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA iso TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA iso GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA iso GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA iso GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
+
