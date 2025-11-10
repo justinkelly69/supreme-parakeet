2c8467c63f8b00812b9d85f5522f63295a84612c
Justin Kelly <justinkelly@justinkelly.net>
2025-11-10 21:07:23 +0000

    insert into attractions

--- apify/insert.pl
+++ apify/insert.pl
@@ -1,28 +1,76 @@
 #! /usr/bin/env perl
 use 5.34.0;
 use JSON::Parse 'parse_json', 'assert_valid_json', 'read_json';
+use JSON qw(encode_json);
 use Data::Dumper;
 use Data::Dump qw(dump);
+use Scalar::Util qw(looks_like_number);
 
-my $jsonFile = './all-dublin.json';
+my $jsonFile = './dublin-museums.json';
 my $insert   = './insert.sql';
+my @values;
 
 open(my $fh, "<", "$jsonFile") or die "Can't open < $jsonFile: $!";
     my $json = read_json($jsonFile);
 
     #say Dumper($json);
     for my $place (@$json) {
-        say insertPlace($place, 'IE-dublin');
+        push(@values, insertPlace($place, 'IE-dublin'));
         #say Dumper($place);
     }
 close($fh);
+
+my $insert = 'INSERT INTO "world"."attractions" (
+        "city_id",
+        "title",
+        "subtitle",
+        "description",
+        "price",
+        "category_name",
+        "address",
+        "neighborhood",
+        "street",
+        "postal_code",
+        "state",
+        "city",
+        "country",
+        "website",
+        "phone",
+        "phone_unformatted",
+        "latitude",
+        "longitude",
+        "located_in",
+        "total_score",
+        "permanently_closed",
+        "temporarily_closed",
+        "place_id",
+        "categories",
+        "scraped_at",
+        "opening_hours",
+        "additional_info",
+        "image_url"
+) VALUES ';
+
+say $insert . join(',', @values) . ';';
 
 sub escape_quot {
     my($str) = @_;
 
     my @str = split(m!'!, $str);
-    my $out = join('@', @str);
+    my $out = join('&apos;', @str);
     return $out;
+}
+
+sub array_quot {
+    my ($arr) = @_;
+    my @out;
+#say("IN:", encode_json($arr));
+    foreach $a (@{$arr}) {
+        #say("a=$a");
+        push(@out, escape_quot($a))
+    }
+    #say("OUT:", encode_json(\@out));
+    return \@out;
 }
 
 sub info {
@@ -30,32 +78,45 @@ sub info {
     my %out;
 
     foreach my $key1 (keys %$arr1) {
-        $out{$key1} = [];
+        my $k1 = escape_quot($key1);
+        $out{$k1} = [];
         my $arr2 = $arr1->{$key1};
+        #say "OUT 1: $k1";
 
         for (my $i = 0; $i < @$arr2; $i++) {
+            #say "OUT 2: @$arr2";
             foreach my $key3 (keys %{$$arr2[$i]}) {
-                push(@{$out{$key1}}, $key3);
+                my $k3 = escape_quot($key3);
+                push(@{$out{$k1}}, $k3);
+                #say "OUT 3: $k1 -> $k3";
             }
         }
     }
     #say 'out:' . Dumper(\%out);
-    $Data::Dumper::Purity = 1;
+    #$Data::Dumper::Purity = 1;
 
-    return Dumper(\%out);
+    return \%out;
+}
+
+sub isNumeric {
+    my($number) = @_;
+    return looks_like_number($number) ? $number : 0;
+}
+
+sub isBoolean {
+    my($bool) = @_;
+    return $bool ? 'TRUE' : 'FALSE',
 }
 
 sub insertPlace {
     my($place, $city_id) = @_;
-
-    #say 'OUT:', Dumper(info($place->{'additionalInfo'}));
 
     my $insertPlace = {
         'city_id'               =>  $city_id,
         'title'                 =>  escape_quot($place->{'title'}),
         'subtitle'              =>  escape_quot($place->{'subTitle'}),
         'description'           =>  escape_quot($place->{'description'}),
-        'price'                 =>  $place->{'price'},
+        'price'                 =>  isNumeric($place->{'price'}),
         'category_name'         =>  $place->{'categoryName'},
         'address'               =>  escape_quot($place->{'address'}),
         'neighborhood'          =>  escape_quot($place->{'neighborhood'}),
@@ -67,17 +128,17 @@ sub insertPlace {
         'website'               =>  $place->{'website'},
         'phone'                 =>  $place->{'phone'},
         'phone_unformatted'     =>  $place->{'phoneUnformatted'},
-        'latitude'              =>  $place->{'location'}->{'lat'},
-        'longitude'             =>  $place->{'location'}->{'lng'},
+        'latitude'              =>  $place->{'location'}->{'lat'} || 0,
+        'longitude'             =>  $place->{'location'}->{'lng'} || 0,
         'located_in'            =>  escape_quot($place->{'locatedIn'}),
         'total_score'           =>  $place->{'totalScore'},
-        'permanently_closed'    =>  $place->{'permanentlyClosed'},
-        'temporarily_closed'    =>  $place->{'temporarilyClosed'},
+        'permanently_closed'    =>  isBoolean($place->{'permanentlyClosed'}),
+        'temporarily_closed'    =>  isBoolean($place->{'temporarilyClosed'}),
         'place_id'              =>  $place->{'placeId'},
-        'categories'            =>  dump($place->{'categories'}),
+        'categories'            =>  encode_json(array_quot($place->{'categories'})),
         'scraped_at'            =>  $place->{'scrapedAt'},
-        'opening_hours'         =>  dump($place->{'openingHours'}),
-        'additional_info'       =>  info($place->{'additionalInfo'}),
+        'opening_hours'         =>  encode_json($place->{'openingHours'}),
+        'additional_info'       =>  encode_json(info($place->{'additionalInfo'})),
         'image_url'             =>  $place->{'imageUrl'}
     };
 
@@ -88,7 +149,7 @@ sub insertPlace {
         '$insertPlace->{'title'}',
         '$insertPlace->{'subtitle'}',
         '$insertPlace->{'description'}',
-        '$insertPlace->{'price'}',
+        $insertPlace->{'price'},
         '$insertPlace->{'categoryName'}',
         '$insertPlace->{'address'}',
         '$insertPlace->{'neighborhood'}',
@@ -100,18 +161,18 @@ sub insertPlace {
         '$insertPlace->{'website'}',
         '$insertPlace->{'phone'}',
         '$insertPlace->{'phone_unformatted'}',
-        '$insertPlace->{'location'}->{'lat'}',
-        '$insertPlace->{'location'}->{'lng'}',
+        $insertPlace->{'latitude'},
+        $insertPlace->{'longitude'},
         '$insertPlace->{'located_in'}',
         '$insertPlace->{'total_score'}',
-        '$insertPlace->{'permanently_closed'}',
-        '$insertPlace->{'temporarily_closed'}',
+        $insertPlace->{'permanently_closed'},
+        $insertPlace->{'temporarily_closed'},
         '$insertPlace->{'place_id'}',
         '$insertPlace->{'categories'}',
         '$insertPlace->{'scraped_at'}',
         '$insertPlace->{'opening_hours'}',
         '$insertPlace->{'additional_info'}',
-        '$insertPlace->{'image_url'}',
+        '$insertPlace->{'image_url'}'
     )
 EOF
     return $insertLine;
